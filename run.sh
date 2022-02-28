8feb51e83dccbfdaeeeb41857a6fe9b9c1d45644
Lyu Jiafei <45746798+dmksjfl@users.noreply.github.com>
2022-02-28 13:05:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 for ((i=1;i<6;i+=1))
 do 
-	python main.py --env Ant-v2 --save-model --policy DARC --dir './logs/DARC/Ant/r'$i --seed $i --qweight 0.22
+	python main.py --env Ant-v2 --save-model --policy DARC --dir './logs/DARC/Ant/r'$i --seed $i --qweight 0.25
 	python main.py --env HalfCheetah-v2 --save-model --policy DARC --dir './logs/DARC/HalfCheetah/r'$i --seed $i --qweight 0.1
-	python main.py --env Walker2d-v2 --save-model --policy DARC --dir './logs/DARC/Walker2d/r'$i --seed $i --qweight 0.12
+	python main.py --env Walker2d-v2 --save-model --policy DARC --dir './logs/DARC/Walker2d/r'$i --seed $i --qweight 0.1
 	python main.py --env Hopper-v2 --save-model --policy DARC --dir './logs/DARC/Hopper/r'$i --seed $i --qweight 0.15
 	python main.py --env BipedalWalker-v3 --save-model --policy DARC --dir './logs/DARC/BipedalWalker/r'$i --seed $i --qweight 0.4
 	python main.py --env AntMuJoCoEnv-v0 --save-model --policy DARC --dir './logs/DARC/AntPybullet/r'$i --seed $i --qweight 0.2
